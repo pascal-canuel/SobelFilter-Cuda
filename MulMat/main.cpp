1a74272c1f2015215e6d7abacb4d75d408d2baca
Pascal Canuel <pascalcanuel@hotmail.com>
2018-09-05 15:08:35 -0400

    Update main.cpp

--- MulMat/main.cpp
+++ MulMat/main.cpp
@@ -1,4 +1,4 @@
-// MulMat.cpp : définit le point d'entrée pour l'application console.
+// MulMat.cppÂ : dÃ©finit le point d'entrÃ©e pour l'application console.
 //
 
 #include "stdafx.h"
@@ -11,12 +11,58 @@
 #include <opencv2/highgui/highgui.hpp> 
 #include <opencv2/imgproc/types_c.h> 
 #include <opencv2/imgproc/imgproc.hpp> 
+#include <cmath>
 
 using namespace cv;
 
 extern "C" cudaError_t Launcher_ScalaireMulMat_Int(uchar *pMatI, int K, uchar *pMatO, dim3 DimMat);
+extern  float TempsExecution; //	TODO start timer
 
-extern  float TempsExecution;
+int Gx[3][3] = {{-1, 0, 1},
+				{-2, 0, 2},
+				{-1, 0, 1}};
+
+int Gy[3][3] = {{-1, -2, -1},
+				{0, 0, 0},
+				{1, 2, 1}};
+
+int gradientX(Mat pImg, Point pPos) {
+	int total = 0;
+	for (int y = 0; y < 3; y++) {
+		for (int x = 0; x < 3; x++) {
+			int i = pPos.y + y - 1;
+			int j = pPos.x + x - 1;
+			int current = (int)pImg.at<uchar>(i, j);
+			int g = current * Gx[y][x];
+			total += g;
+		}
+	}
+
+	return total;
+}
+
+int gradientY(Mat pImg, Point pPos) {
+	int total = 0;
+	for (int y = 0; y < 3; y++) {
+		for (int x = 0; x < 3; x++) {
+			int i = pPos.y + y - 1;
+			int j = pPos.x + x - 1;
+			int current = (int)pImg.at<uchar>(i, j);
+			int g = current * Gx[y][x];
+			total += g;
+		}
+	}
+	
+	return total;
+}
+
+//int convolutionMat(int *pFiltre, Point pPos) {
+//	
+//}
+
+int approxGradient(int pGradientX, int pGradientY) {
+	return abs(pGradientX) + abs(pGradientY);
+}
 
 int main()
 {
@@ -33,7 +79,27 @@ int main()
 	//	imgOutput.data should point to the modified data
 	imshow("lenaOutput", imgOutput);
 
+	//	Sobel filter on CPU
+	Mat imgSobelCPU = imread(imgPath, 0);
+	for (int y = 1; y < imgSobelCPU.rows - 1; y++) {
+			for (int x = 1; x < imgSobelCPU.cols - 1; x++) {
+				int current = (int)imgSobelCPU.at<uchar>(y, x);
+				Point pos = Point(x, y);
+				int gx = gradientX(imgSobelCPU, pos);
+				int gy = gradientY(imgSobelCPU, pos);
+				int approx = approxGradient(gx, gy);
+				if(approx > 255)
+					imgSobelCPU.at<uchar>(y, x) = 255;
+				else
+					imgSobelCPU.at<uchar>(y, x) = approx;
+				
+			}
+	}
+
+	imshow("SobelCPU", imgSobelCPU);
+
 	waitKey(0);
 	return 0;
 }
+
 
