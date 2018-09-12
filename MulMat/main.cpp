d3c5d309bdd0e032c7f30c9315bef525da88ed1b
Pascal Canuel <pascalcanuel@hotmail.com>
2018-09-11 21:51:50 -0400

    Update main.cpp

--- MulMat/main.cpp
+++ MulMat/main.cpp
@@ -18,6 +18,8 @@ using namespace cv;
 extern "C" cudaError_t Launcher_ScalaireMulMat_Int(uchar *pMatI, int K, uchar *pMatO, dim3 DimMat);
 extern  float TempsExecution; //	TODO start timer
 
+Mat imgSobelCPU;
+
 int Gx[3][3] = {{-1, 0, 1},
 				{-2, 0, 2},
 				{-1, 0, 1}};
@@ -26,42 +28,19 @@ int Gy[3][3] = {{-1, -2, -1},
 				{0, 0, 0},
 				{1, 2, 1}};
 
-int gradientX(Mat pImg, Point pPos) {
-	int total = 0;
+int gradient(int posY, int posX) {
+	int totalX = 0;
+	int totalY = 0;
 	for (int y = 0; y < 3; y++) {
 		for (int x = 0; x < 3; x++) {
-			int i = pPos.y + y - 1;
-			int j = pPos.x + x - 1;
-			int current = (int)pImg.at<uchar>(i, j);
-			int g = current * Gx[y][x];
-			total += g;
+			int current = (int)imgSobelCPU.at<uchar>(posY + y, posX + x);
+			int conX = current * Gx[y][x];
+			int conY = current * Gy[y][x];
+			totalX += conX;
+			totalY += conY;
 		}
 	}
-
-	return total;
-}
-
-int gradientY(Mat pImg, Point pPos) {
-	int total = 0;
-	for (int y = 0; y < 3; y++) {
-		for (int x = 0; x < 3; x++) {
-			int i = pPos.y + y - 1;
-			int j = pPos.x + x - 1;
-			int current = (int)pImg.at<uchar>(i, j);
-			int g = current * Gx[y][x];
-			total += g;
-		}
-	}
-	
-	return total;
-}
-
-//int convolutionMat(int *pFiltre, Point pPos) {
-//	
-//}
-
-int approxGradient(int pGradientX, int pGradientY) {
-	return abs(pGradientX) + abs(pGradientY);
+	return abs(totalX) + abs(totalY);
 }
 
 int main()
@@ -74,29 +53,63 @@ int main()
 
 	//	Call kernel launcher
 	int k = 50;
-	Launcher_ScalaireMulMat_Int(imgInput.data, k, imgOutput.data, dim3(imgInput.rows, imgOutput.cols));
+	//Launcher_ScalaireMulMat_Int(imgInput.data, k, imgOutput.data, dim3(imgInput.rows, imgOutput.cols));
 
 	//	imgOutput.data should point to the modified data
-	imshow("lenaOutput", imgOutput);
+	//imshow("lenaOutput", imgOutput);
 
 	//	Sobel filter on CPU
-	Mat imgSobelCPU = imread(imgPath, 0);
-	for (int y = 1; y < imgSobelCPU.rows - 1; y++) {
-			for (int x = 1; x < imgSobelCPU.cols - 1; x++) {
-				int current = (int)imgSobelCPU.at<uchar>(y, x);
-				Point pos = Point(x, y);
-				int gx = gradientX(imgSobelCPU, pos);
-				int gy = gradientY(imgSobelCPU, pos);
-				int approx = approxGradient(gx, gy);
-				if(approx > 255)
-					imgSobelCPU.at<uchar>(y, x) = 255;
-				else
-					imgSobelCPU.at<uchar>(y, x) = approx;
-				
-			}
+	imgSobelCPU = imread(imgPath, 0);
+	
+	int size = (imgSobelCPU.rows - 2) * (imgSobelCPU.cols - 2);
+	int *gradTotal = new int[size];
+	for (int y = 0; y < imgSobelCPU.rows - 2; y++) {
+		for (int x = 0; x < imgSobelCPU.cols - 2; x++) {
+			int width = imgSobelCPU.cols - 2;
+			int i = width * y + x;
+
+			int g = gradient(y, x);
+			gradTotal[i] = g;
+		}
+	}
+
+	int minVal, maxVal;
+	minVal = *std::min_element(gradTotal, gradTotal + size);
+	maxVal = *std::max_element(gradTotal, gradTotal + size);
+	
+	// mapper int entre 0 et 255
+	for (int y = 0; y < imgSobelCPU.rows - 2; y++) {
+		for (int x = 0; x < imgSobelCPU.cols - 2; x++) {
+
+			int width = imgSobelCPU.cols - 2;
+			int i = width * y + x;
+			int current = gradTotal[i];
+
+			int map = (current * 255) / maxVal;
+
+			imgSobelCPU.at<uchar>(y, x) = map;
+		}
 	}
 
 	imshow("SobelCPU", imgSobelCPU);
+
+	//	Expected Sobel
+	Mat src_gray = imread(imgPath, 0);
+	Mat grad;
+	int scale = 1;
+	int delta = 0;
+	Mat grad_x, grad_y;
+	Mat abs_grad_x, abs_grad_y;
+	int ddepth = CV_16S;
+
+	/// Gradient X
+	Sobel(src_gray, grad_x, ddepth, 1, 0, 3, scale, delta, BORDER_DEFAULT);
+	/// Gradient Y
+	Sobel(src_gray, grad_y, ddepth, 0, 1, 3, scale, delta, BORDER_DEFAULT);
+	convertScaleAbs(grad_x, abs_grad_x);
+	convertScaleAbs(grad_y, abs_grad_y);
+	addWeighted(abs_grad_x, 0.5, abs_grad_y, 0.5, 0, grad);
+	imshow("ExpectedSobel", grad);
 
 	waitKey(0);
 	return 0;
